125037dc11b44f3fc71f50f984fbe66f6722d910
Sheby <83452219+Shebyyy@users.noreply.github.com>
2026-06-05 08:35:10 +0530

    Redirect to '/repos' instead of '/extensions'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react'
 
 export default function Home() {
   useEffect(() => {
-    window.location.replace('/extensions')
+    window.location.replace('/repos')
   }, [])
 
   return (
